91418ae63ae72788be9d9cb73d608b3a3339497a
Florian Breitwieser <florian.bw@gmail.com>
2017-09-12 13:33:18 -0400

    Added tmux

--- brew.sh
+++ brew.sh
@@ -80,7 +80,7 @@ brew install ack
 #brew install exiv2
 brew install git
 brew install git-lfs
-brew install gpg
+brew install tmux
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
